67a5ff2d289f89a93a20de66cd9eeb519fb49c4b
DaryaYa <lyalyaka@mail.ru>
2019-10-11 18:31:23 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let n = str.length;
+
+    if (n % 2 != 0 || n == 0) {
+        return false;
+    }
+
+    let strArray = str.split('');
+
+    for (let i = 0; i < strArray.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let open = bracketsConfig[j][0];
+            let closed = bracketsConfig[j][1];
+
+            if (strArray[i] == open && strArray[i + 1] == closed) {
+                strArray.splice(i, 2);
+                i -= 2;
+            }
+        }
+    }
+
+    return (strArray.length == 0) ? true : false
+
+
 }
